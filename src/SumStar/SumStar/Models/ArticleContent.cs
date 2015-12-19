ff689809c0b59bb8362221bae683ac32f0220a65
王睿 <wangrui821@hotmail.com>
2015-12-19 13:43:53 +0800

    补充提交遗漏的文件

---
+++ src/SumStar/SumStar/Models/ArticleContent.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SumStar.Models
+{
+	[DisplayName("文章内容")]
+	[Table("T_ArticleContent")]
+	[DisplayColumn("Title")]
+	public class ArticleContent : Content
+	{
+		[Display(Name = "题图")]
+		[StringLength(200)]
+		public string TopicImage
+		{
+			get;
+			set;
+		}
+
+		[Display(Name = "作者")]
+		[StringLength(20)]
+		public string Author
+		{
+			get;
+			set;
+		}
+
+		[Required]
+		[Display(Name = "内容")]
+		[Column(TypeName = "text")]
+		[DataType(DataType.MultilineText)]
+		public string Content
+		{
+			get;
+			set;
+		}
+	}
+}
---
+++ src/SumStar/SumStar/Models/ImageContent.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SumStar.Models
+{
+	[DisplayName("图片内容")]
+	[Table("T_ImageContent")]
+	[DisplayColumn("Title")]
+	public class ImageContent : Content
+	{
+		[Required]
+		[Display(Name = "链接地址")]
+		[StringLength(200)]
+		public string LinkUrl
+		{
+			get;
+			set;
+		}
+
+		[Required]
+		[Display(Name = "图片地址")]
+		[StringLength(200)]
+		public string ImageUrl
+		{
+			get;
+			set;
+		}
+	}
+}
